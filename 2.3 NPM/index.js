b6a96f73110ffeb92af822c53358289c91db85bd
yamurb <yagmurbaycol@gmail.com>
2026-07-23 00:54:13 +0300

    learning how to use npm

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,10 @@
+//import generateName from 'sillyname';
+//let sillyName = generateName();
+
+//console.log(`my name is ${sillyName}`);
+
+
+import { randomSuperhero } from "superheroes";
+
+let heroname = randomSuperhero();
+console.log(`my hero name is ${heroname}!`);
